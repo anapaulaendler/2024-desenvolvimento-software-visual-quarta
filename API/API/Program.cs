9ca5083fab70ab6af555f603930af151dd2b4878
Auto Logon <autologon@CSED.LAB>
2024-09-18 10:47:15 -0300

    final de aula

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -5,6 +5,7 @@
 //MINIMAL APIs - C# - Minimal APIs
 
 using API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -45,19 +46,37 @@ app.MapGet("/", () => "API de Produtos");
 //GET: /produto/listar
 app.MapGet("/produto/listar", () =>
 {
-    return Results.Ok(produtos);
+    if (produtos.Count > 0)
+    {
+        return Results.Ok(produtos);
+    }
+    return Results.NotFound();
 });
 
+app.MapGet("/produto/buscar/{nome}", (string nome) => {
+    foreach(Produto produtoCadastrado in produtos)
+    {
+        if (produtoCadastrado.Nome == nome)
+        {
+            return Results.Ok(produtoCadastrado);
+        }
+    }
+    return Results.NotFound();
+});
 //POST: /produto/cadastrar
-app.MapPost("/produto/cadastrar/{nome}", 
-    (string nome) =>
+app.MapPost("/produto/cadastrar", 
+    ([FromBody] Produto produto) =>
 {
+    produtos.Add(produto);
+    return Results.Created("", produto);
+    /*
     //Criar o objeto e preencher
     Produto produto = new Produto();
     produto.Nome = nome;
     //Adicionando dentro da lista
     produtos.Add(produto);
     return Results.Ok(produtos);
+    */
 });
 
 //Criar um funcionalidade para receber informações
@@ -81,3 +100,7 @@ app.Run();
 // produto.setPreco(5);
 // Console.WriteLine("Preço: " + 
 //     produto.getPreco());
+
+/* exercicios:
+    remover produto 
+    aterar produto */
